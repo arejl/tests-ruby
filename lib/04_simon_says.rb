4e1bb87703b716c77a68218ca3336cbc9f202c76
Ariane <pikabooariane@hotmail.fr>
2021-01-18 12:48:00 +0200

    Ajout exercice 4

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -12,6 +12,14 @@ def repeat(string,*n)
 end
 
 def start_of_word(string,n)
-    
+    return string[0..n-1]
 end
 
+def first_word(string)
+    return string.split(' ').first
+end
+
+def titleize(string)
+    little_words=["a", "an", "and", "the", "or", "for", "of", "nor"]
+    return string.split(' ').each_with_index.map{|word, index| little_words.include?(word) && index > 0 ? word : word.capitalize }.join(' ')
+end
